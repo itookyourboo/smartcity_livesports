0950d851d079f8011874f6be333c9a6acf9c6c21
itookyourboo <wignorbo@yandex.ru>
2022-07-15 02:38:00 +0300

    Added ScrollView and fixed schedule

--- src/Screens/ScheduleScreen.js
+++ src/Screens/ScheduleScreen.js
@@ -1,5 +1,10 @@
-import { color } from "@rneui/base";
-import {Text, View, StyleSheet, Button} from "react-native";
+import {color} from "@rneui/base";
+import {Text, Button, Dialog} from "@rneui/themed";
+import {View, StyleSheet, ScrollView} from "react-native";
+import {useState} from "react";
+
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus corporis cupiditate deleniti " +
+    "dolores dolorum eos eum hic id illo labore magni, maiores minus molestiae nam nihil, obcaecati quibusdam quo sit?"
 
 const compl = [
     {date: "7 августа", sport: 'Теннис'},
@@ -9,29 +14,45 @@ const compl = [
 // random color
 
 function ScheduleScreen({navigation}) {
+    const [openDialog, setOpenDialog] = useState({visible: false});
+
+    function toggleDialog(props) {
+        setOpenDialog({...openDialog, ...props, visible: !openDialog.visible});
+    }
+
     return (
-        <View style={{alignItems: 'flex-start', justifyContent: 'center'}}>
-            {
-            compl.map(({date, sport}) => (
-            <View style={style.block}> 
-                <View style={style.date}>
-                    <Text style={style.num}>{date}</Text>
-                </View>
-                <View style={style.more}>
-                    <Text style={style.desc}>{sport}</Text>
-                    <Button type="outline" style={style.btn} title="Подробнее" onPress={() => toggleDialog(props)}/>
-                </View>
+        <ScrollView>
+            <View style={{flex: 1, marginBottom: 16}}>
+                <Dialog
+                    isVisible={openDialog.visible}
+                    onBackdropPress={toggleDialog}>
+                    <Dialog.Title title={openDialog.sport || 'Волейбол'}/>
+                    <Text style={{marginBottom: 10}}> {openDialog.date || '15 июля'} </Text>
+                    <Text> {lorem} </Text>
+                </Dialog>
+                {
+                    compl.map(({date, sport}) => (
+                        <View key={sport} style={style.block}>
+                            <View style={style.date}>
+                                <Text style={style.num}>{date}</Text>
+                            </View>
+                            <View style={style.more}>
+                                <Text style={style.desc}>{sport}</Text>
+                                <Button type="outline" style={style.btn} title="Подробнее"
+                                        onPress={() => toggleDialog({date, sport})}/>
+                            </View>
+                        </View>
+                    ))
+                }
             </View>
-            ))
-            }
-        </View>
+        </ScrollView>
     );
 }
 
 const style = StyleSheet.create({
     block: {
         // backgroundColor: '#7AC1F8',
-        marginTop: 20,
+        marginTop: 16,
         alignSelf: 'stretch',
         padding: 15,
         marginHorizontal: 10,
