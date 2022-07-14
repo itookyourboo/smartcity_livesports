dee4c09b42f60a13939b14471579d177d9a254e9
itookyourboo <wignorbo@yandex.ru>
2022-07-14 18:00:22 +0300

    Event list with cards

--- src/Screens/EventsScreen.js
+++ src/Screens/EventsScreen.js
@@ -1,10 +1,33 @@
-import {Text, View} from "react-native";
+import {ScrollView, View} from "react-native";
+import {Button, Text, Card} from "@rneui/themed";
+
+
+const events = [
+    {title: 'Волейбол'},
+    {title: 'Настольный теннис'},
+    {title: 'Баскетбол'},
+    {title: 'Бадминтон'},
+    {title: 'Многоборье ГТО'},
+    {title: 'Шахматы'},
+    {title: 'Плавание'},
+]
 
 function EventsScreen({navigation}) {
     return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Events Screen</Text>
-        </View>
+        <ScrollView>
+            <View style={{flex: 1, marginBottom: 16}}>
+                <View>
+                    {events.map((props) => (
+                        <Card key={props.title}>
+                            <Card.Title>{props.title || 'Title'}</Card.Title>
+                            <Card.Divider/>
+                            <Text style={{marginBottom: 10}}>{props.description || 'Описание мероприятия'}</Text>
+                            <Button type="outline" title="Подробнее"></Button>
+                        </Card>
+                    ))}
+                </View>
+            </View>
+        </ScrollView>
     );
 }
 
